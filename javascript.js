8eff2eaa221e909f7157a71cf0a6c5b2790c05c0
jayjvaughn <waveyjxyshop@gmail.com>
2024-05-09 15:04:20 -0400

    basic first layout of my calculator
    Not at all perfect, i need to revisit flexbox materials to better organize this so that its cleaner and dynamic.

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    let c = a + b;
+    return c;
+};
+
+function subtract(a, b) {
+    let c = a - b;
+    return c;
+};
+
+function multiply(a, b) {
+    let c = a * b;
+    return c;
+};
+
+function divide(a, b) {
+    let c = a / b;
+    return c;
+};
+divide(20, 5);
+
+
