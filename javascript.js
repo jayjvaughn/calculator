347ebd075d695c8b3c9dd706bb2d6564a9e55d58
jayjvaughn <waveyjxyshop@gmail.com>
2024-05-15 17:41:38 -0400

    not usable code
    currently experimenting with ways to shorten my code because its kind of a lot, im trying to create a function that i can use for all my numbers to display and store them. I also added a few more buttons like the equals and a fun little dont push button

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+let num1 = null;
+let operator = null;
+let num2 = null;
+
+function operate(){
+
+};
+
 function add(a, b) {
     let c = a + b;
     return c;
@@ -19,11 +27,71 @@ function divide(a, b) {
 };
 divide(20, 5);
 
-// --constants-- all my button functionality
-const total = document.querySelector("#totalDisplay");
-const one = document.querySelector("#one");
-one.addEventListener("click", ()=> {
-total.textContent = 1;
-return 1;
-});
+function displayThisNumber(input){
+    totalDisplay.value += input;
+}
 
+// --constants-- all my number button functionality
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", displayThisNumber("") 
+);
+const total = document.querySelector("#totalDisplay");
+
+const one = document.querySelector("#one");
+one.onclick = displayThisNumber(1);
+
+const two = document.querySelector("#two");
+two.addEventListener("click", ()=> {
+total.textContent = 2;
+return 2;
+});
+const three = document.querySelector("#three");
+three.addEventListener("click", ()=> {
+total.textContent = 3;
+return 3;
+});
+const four = document.querySelector("#four");
+four.addEventListener("click", ()=> {
+total.textContent = 4;
+return 4;
+});
+const five = document.querySelector("#five");
+five.addEventListener("click", ()=> {
+total.textContent = 5;
+return 5;
+});
+const six = document.querySelector("#six");
+six.addEventListener("click", ()=> {
+total.textContent = 6;
+return 6;
+});
+const seven = document.querySelector("#seven");
+seven.addEventListener("click", ()=> {
+total.textContent = 7;
+return 7;
+});
+const eight = document.querySelector("#eight");
+eight.addEventListener("click", ()=> {
+total.textContent = 8;
+return 8;
+});
+const nine = document.querySelector("#nine");
+nine.addEventListener("click", ()=> {
+total.textContent = 9;
+return 9;
+});
+const zero = document.querySelector("#zero");
+zero.addEventListener("click", ()=> {
+total.textContent = 0;
+return 0;
+});
+const decimal = document.querySelector("#decimal");
+decimal.addEventListener("click", ()=> {
+total.textContent = ".";
+return ".";
+});
+const dontPush = document.querySelector("#fun");
+dontPush.addEventListener("click", ()=> {
+total.textContent = "80085";
+return "80085";
+});
